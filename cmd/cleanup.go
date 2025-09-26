78458515038aa009a5fa4eef9e7d242b9b60df16
Arttu Olli <60509537+ArttuOll@users.noreply.github.com>
2025-09-26 09:43:06 +0300

    Open given epub and print names of its files

---
+++ cmd/cleanup.go
@@ -0,0 +1,23 @@
+package cmd
+
+import (
+	"archive/zip"
+	"fmt"
+)
+
+func Cleanup(path string) error {
+	r, err := zip.OpenReader(path)
+	if err != nil {
+		return fmt.Errorf("failed to open epub file: %w", err)
+	}
+
+	defer r.Close()
+
+	// Iterate through the files in the archive,
+	// printing some of their contents.
+	for _, f := range r.File {
+		fmt.Printf("Files contained by the given epub %s:\n", f.Name)
+	}
+
+	return nil
+}
